7eb94eb625e99b9c59d69fff9571d68fe2c347e9
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-03 16:37:37 +0900

    １０章、５

---
+++ ch8/src/ch10/ch10_5.java
@@ -0,0 +1,22 @@
+package ch10;
+
+//Heroを継承したクラスSupreHeroを作成
+class SuperHer extends Hero {
+	//フィールドの追加　
+	boolean flying;
+	
+	//メソッド　「飛ぶ」「着地」を追加
+	public void fly() {
+		this.flying = true;
+		System.out.println("飛び上がった！");
+	}
+	
+	public void land() {
+		this.flying = false;
+		System.out.println("着地した！");
+	}
+	//runメソッドを再定義
+	public void run() {
+		System.out.println(this.name + "は逃げだした");
+	}
+}
