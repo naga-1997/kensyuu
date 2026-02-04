8fd791049db164c17ab002fce4627dc77a8f475d
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-04 10:41:55 +0900

    １１章、６

---
+++ ch8/src/ch11/ch11_6.java
@@ -0,0 +1,12 @@
+package ch11;
+
+//継承（Character）の材料をnewしてしまった場合
+ class Main {
+	 public static void main(String[] args) {
+		 Character c = new Character();
+		 Matango m = new Matango();
+		 
+		 c.attack(m);
+	 }
+}
+ //結果何も起きない
