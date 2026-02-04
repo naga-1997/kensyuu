0992ca397eb105e8c441ca08aea0a9f1e85ca47e
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-04 16:08:06 +0900

    １１章、練習問題１　改

--- ch8/src/ch11/ch11_Refactoring.java
+++ ch8/src/ch11/ch11_Refactoring.java
@@ -7,6 +7,13 @@ abstract class TangibleAsset{
 	int price;
 	String color;
 	
+	//コンストラクタ
+	public TangibleAsset(String name,int price,String color) {
+		this.name = name;
+		this.price = price;
+		this.color = color;
+	}
+	
 	//メソッド
 	public String getName() {return this.name;}
 	public int getPrice() {return this.price;}
@@ -17,6 +24,12 @@ abstract class TangibleAsset{
 class Book1 extends TangibleAsset{
 	//追加のメンバ変数
 	String isbn;
+	
+	//コンストラクタ
+	public Book1(String name,int price,String color,String isbn) {
+		super(name, price, color);
+		this.isbn = isbn;
+	}
 	//追加のメソッド
 	public String getisbn() {return this.isbn;}
 }
@@ -24,6 +37,12 @@ class Book1 extends TangibleAsset{
 class Computer1 extends TangibleAsset{
 	//追加のメンバ変数
 	String makerName;
+	
+	//コンストラクタ
+	public Computer1(String name,int price,String color,String makerName) {
+		super(name, price, color);
+		this.makerName = makerName;
+	}
 	
 	//追加のメソッド
 	public String getMakerName() {return this.makerName;}
