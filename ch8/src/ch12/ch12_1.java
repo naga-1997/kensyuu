e89a9d687b60171144f691dbe1948e5dc8abf104
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-04 22:41:40 +0900

    １２章、１

---
+++ ch8/src/ch12/ch12_1.java
@@ -0,0 +1,13 @@
+package ch12;
+
+interface Life{}
+
+class Wizard implements Life{}
+
+class Main{
+	public static void main(String[] args) {
+		//Life型の変数にWizardを代入することができる
+		Life lf = new Wizard();
+	}
+}
+
