bdf879f53d7632340a447155ad2adbc2d2b3f0f9
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-04 10:35:56 +0900

    １１章、４

---
+++ ch8/src/ch11/ch11_4.java
@@ -0,0 +1,16 @@
+package ch11;
+
+//Characterクラスの継承
+//オーバーライドをしない（忘れた場合のコード）
+class Hero extends Character {
+}
+
+//オーバーライドをしない（忘れた場合のコード）場合のメインメソッド
+class Main{
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		Matango m = new Matango();
+		
+		h.attack(m);
+  	}
+}
