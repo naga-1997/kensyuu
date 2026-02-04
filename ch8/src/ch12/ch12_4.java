9f6e224dde471b188170fa78358d6338fb1f9507
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-04 23:09:59 +0900

    １２章、４

---
+++ ch8/src/ch12/ch12_4.java
@@ -0,0 +1,14 @@
+package ch12;
+
+class Main {
+
+	public static void main(String[] args) {
+		Wizard w = new Wizard();
+		Character c = new Wizard();
+		Matango m = new Matango();
+		
+		c.name = "アサカ";
+		c.attack(m);
+		c.fireball(m);//エラー　cの中身がHeroかWizardかわからない為
+	}
+}
