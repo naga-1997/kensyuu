05e67d76aa9f17aaf20baf0a30f53b008acd1716
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-05 10:36:58 +0900

    １２章、８

---
+++ ch8/src/ch12/ch12_8.java
@@ -0,0 +1,17 @@
+package ch12;
+
+class Hero extends Character{
+	//キノコに対する攻撃
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃」！");
+		System.out.println("敵に１０ポイントのダメージをあたえた！");
+		m.hp -= 10;
+	}
+	//ゴブリンに対する攻撃
+	public void attack(Goblin g) {
+		System.out.println(this.name + "の攻撃」！");
+		System.out.println("敵に１０ポイントのダメージをあたえた！");
+		g.hp -= 10;
+	}
+	//以下スライムなど続く。。。
+}
