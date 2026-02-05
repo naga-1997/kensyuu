0fc6b39f7245a8390886c0947cb1df3165fb2fc0
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-05 12:26:37 +0900

    １３章、５

---
+++ ch8/src/ch13/ch13_5.java
@@ -0,0 +1,16 @@
+package ch13;
+
+class Hero {
+	 private int hp;
+	 String name;
+	 Sword sword;
+	 
+	 public void bye() {
+		 System.out.println("勇者は別れを告げた");
+	 }
+	 
+	 private void die() {
+		 System.out.println(this.name + "は死んでしまった！");
+		 System.out.println("GAMEOVERです。");
+	 }
+}
