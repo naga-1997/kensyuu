d6dfa0b2fab9f1919759797d988a2736b3fd407b
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-05 11:17:17 +0900

    １２章、１０

---
+++ ch8/src/ch12/ch12_10.java
@@ -0,0 +1,17 @@
+package ch12;
+/*************************
+ * それぞれのモンスターが逃げる
+ * それぞれのクラスはch12_2にある
+ *************************/
+class Main {
+	public static void main(String[] args) {
+		Monster[] monsters = new Monster[3];
+		monsters[0] = new Slime();
+		monsters[1] = new Goblin();
+		monsters[2] = new DeathBat();
+		
+		for(Monster m : monsters) {
+			m.run();
+		}
+	}
+}
