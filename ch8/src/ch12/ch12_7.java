3183a7ae226af14c459c06b9d964227be2a7131f
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-05 10:27:25 +0900

    １２章、７

---
+++ ch8/src/ch12/ch12_7.java
@@ -0,0 +1,19 @@
+package ch12;
+
+class Main {
+
+	public static void main(String[] args) {
+		Character[] c = new Character[5];
+		c[0] = new Hero();
+		c[1] = new Hero();
+		c[2] = new Thief();
+		c[3] = new Wizard();
+		c[4] = new Wizard();
+		
+		//宿舎に泊まる
+		for (Character ch : c) {
+			ch.hp += 50;
+		}
+	}
+
+}
