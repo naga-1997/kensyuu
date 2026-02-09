c1a136803e678fe5836651306fd5df3b21304c60
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 18:31:55 +0900

    １７章、３

---
+++ ch8/src/ch17/ch17_3.java
@@ -0,0 +1,17 @@
+package ch17;
+
+import java.io.FileWriter;
+
+public class ch17_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		try {
+			FileWriter fw = new FileWriter("date.txt");
+			fw.write("hello!");
+			fw.close();
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		}
+	}
+}
