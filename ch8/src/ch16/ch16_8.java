7990b6e2c743ba158c13cc9aa35b245b74685d19
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 16:35:14 +0900

    １６章、８

---
+++ ch8/src/ch16/ch16_8.java
@@ -0,0 +1,18 @@
+package ch16;
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero {
+	public String name;
+}
+
+class Main{
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		List<Hero> list = new ArrayList<>();
+		list.add(h);
+		h.name = "スガワラ";
+		System.out.println(list.get(0).name);
+	}
+}
