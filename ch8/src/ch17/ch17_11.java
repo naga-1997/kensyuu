f5e6fb6b5125f08092d00f776cd6dece5c1f8295
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 22:06:24 +0900

    17章、１１

---
+++ ch8/src/ch17/ch17_11.java
@@ -0,0 +1,18 @@
+package ch17;
+
+class Person{
+	int age;
+	public void setAge(int age) {
+		if(age <0) {
+			throw new IllegalArgumentException("年齢は０歳以上の数を指定すべきです。指定値＝" + age);
+		}
+		this.age = age;
+	}
+}
+
+class Main{
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.setAge(-100);
+	}
+}
