2d9e256bfc3a3d674f974b6b7ba7f6fc0e7ee1ec
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 15:49:44 +0900

    １６章、１〜３

---
+++ ch8/src/ch16/ch16_3.java
@@ -0,0 +1,17 @@
+package ch16;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+
+	public static void main(String[] args) {
+		Set<String> colors = new HashSet<String>();
+		colors.add("赤");
+		colors.add("青");
+		colors.add("黄");
+		colors.add("赤");
+		System.out.println();
+	}
+
+}
---
+++ ch8/src/ch16/ch16_sample.java
@@ -0,0 +1,18 @@
+package ch16;
+/*
+import java.util.ArrayList;
+
+class Main{
+	public static void main(String[] args) {
+	//ArrarListを準備
+	ArrayList<String> names = new ArrayList<String>(); 
+	
+	//3人を追加
+	names.add("たなか");
+	names.add("すずき");
+	names.add("さいとう");
+	
+	System.out.println(names.get(1));
+	}
+}
+*/
