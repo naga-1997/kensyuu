663000c07456e2d8d6579f1bfa53b18f5234d136
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 16:53:30 +0900

    １６章練習問題２

---
+++ ch8/src/ch16/ch16_Question2.java
@@ -0,0 +1,30 @@
+package ch16;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero{
+	private String name;
+	public Hero(String name) {this.name = name;}
+	public String getName() {return this.name;}
+}
+
+
+
+/*************************
+ * 斉藤、鈴木をHeroとしてインスタンスして
+ * ArrayListに格納
+ * １人ずつ名前を表示
+ *************************/
+class Main {
+	public static void main(String[] args) {
+		Hero h1 = new Hero("斉藤");
+		Hero h2 = new Hero("鈴木");
+		List<Hero> list = new ArrayList<Hero>();
+		list.add(h1);
+		list.add(h2);
+		for(Hero s : list) {
+			System.out.println(s.getName() + "さん");
+		}
+	}
+}
