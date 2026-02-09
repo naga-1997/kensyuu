8af859eef3b73b192b75bcc4238762f9aed20a1a
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 18:47:10 +0900

    １７章、９

---
+++ ch8/src/ch17/ch17_9.java
@@ -0,0 +1,25 @@
+package ch17;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ch17_9 {
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		try {
+		    fw = new FileWriter("date.txt");
+			fw.write("hello!");
+		} catch (Exception e) {
+			System.out.println("何らかの例外が発生しました");
+		}finally {
+			if (fw != null) {
+			try {
+				fw.close();
+			}catch (IOException e) {
+				;
+			}
+			}
+		}
+	}
+}
+
