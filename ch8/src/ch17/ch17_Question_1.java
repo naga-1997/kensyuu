1c0356bb023a5f8d2cfd6b9828b98edba78b3f33
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 22:28:21 +0900

    １７章、練習問題１、２

---
+++ ch8/src/ch17/ch17_Question_1.java
@@ -0,0 +1,16 @@
+package ch17;
+
+public class ch17_Question_1 {
+	public static void main(String[] args) {
+		String s = null;
+		try {
+		System.out.println(s.length());
+	}catch(NullPointerException e) {
+		System.out.println("NullpointerException例外をcatctしました");
+		System.out.println("ここから");
+		e.printStackTrace();
+		System.out.println("ここまで");
+		
+	}
+}
+}
