b6e71686f6c84ede8c31841c42570df884c45fc5
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 22:34:50 +0900

    17章、練習問題３、４

---
+++ ch8/src/ch17/ch17_Question_3.java
@@ -0,0 +1,10 @@
+package ch17;
+
+public class ch17_Question_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ ch8/src/ch17/ch17_Question_4.java
@@ -0,0 +1,13 @@
+package ch17;
+
+import java.io.IOException;
+
+public class ch17_Question_4 {
+
+	public static void main(String[] args)throws IOException {
+		System.out.println("プログラムが起動しました");
+		throw new IOException();
+
+	}
+
+}
