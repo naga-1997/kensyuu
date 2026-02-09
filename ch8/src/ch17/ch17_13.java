c8f5f000dc87d1f7bc129975437d48b51fa73743
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 22:12:38 +0900

    １７章、１２、１３

---
+++ ch8/src/ch17/ch17_13.java
@@ -0,0 +1,15 @@
+package ch17;
+
+public class ch17_13 {
+
+	public static void main(String[] args) {
+		try {
+			//試験的に例外を発生させる
+			throw new UnsupportedMusicFileException("未対応のファイルです");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
