23f10b89b1b854ae771f52d4b1ffa30caa25d9b7
nagashimakazuki <nagashimakazuki@localhost>
2026-02-09 18:18:52 +0900

    １７章、２

---
+++ ch8/src/ch17/ch17_2.java
@@ -0,0 +1,15 @@
+package ch17;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Main{
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("date.txt");
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			System.out.println("エラーが発生しました。");
+		}
+	}
+}
