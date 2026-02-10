23e969e7632c570c2549fbb37f9904f77f0551b8
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-10 18:37:44 +0900

    ロードマップstep4

--- trainingSpring_Sample_____/src/main/java/com/example/demo/TrainingSpringSampleApplication.java
+++ trainingSpring_Sample_____/src/main/java/com/example/demo/TrainingSpringSampleApplication.java
@@ -1,14 +1,13 @@
 package com.example.demo;
 
-/*import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TrainingspringApplication {
+public class TrainingSpringSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrainingspringApplication.class, args);
+		SpringApplication.run(TrainingSpringSampleApplication.class, args);
 	}
 
 }
-*/
