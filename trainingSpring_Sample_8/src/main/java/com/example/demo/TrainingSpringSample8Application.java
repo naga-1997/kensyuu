ed54101bdd6f63a5e7c98aa631e146052dc79072
nagashimakazuki <nagashimakazuki@nagashiukinoair>
2026-02-11 09:36:48 +0900

    spring ロードマップStep8

---
+++ trainingSpring_Sample_8/src/main/java/com/example/demo/TrainingSpringSample8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSpringSample8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingSpringSample8Application.class, args);
+	}
+
+}
