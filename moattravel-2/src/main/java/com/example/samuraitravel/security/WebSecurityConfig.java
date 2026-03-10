e70bd9aed8e5b2b92259ebad66f1e80b387507fd
Kazuki Nagashima <kazumaru.1925@gmail.com>
2026-03-10 18:21:16 +0900

    ホテルル２週目38章まで

--- moattravel-2/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
+++ moattravel-2/src/main/java/com/example/samuraitravel/security/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig {
 		//どのURLに誰がアクセスできるかを決める
 		http.authorizeHttpRequests((requests) -> requests
 				//全てのユーザーにアクセス許可するURL
-				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**")
+				.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "houses", "houses/{id}")
 				.permitAll()
 				//管理者にのみ許可するURL
 				.requestMatchers("/admin/**").hasRole("ADMIN")
