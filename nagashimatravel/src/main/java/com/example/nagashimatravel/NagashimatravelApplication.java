f60269b0bd1bf89f10e9af33bf9e1abff8468d1c
Kazuki Nagashima <kazumaru.1925@gmail.com>
2026-03-16 18:31:40 +0900

    オリジナルWEBサイト

---
+++ nagashimatravel/src/main/java/com/example/nagashimatravel/NagashimatravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.nagashimatravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagashimatravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagashimatravelApplication.class, args);
+	}
+
+}
---
+++ nagashimatravel/src/test/java/com/example/nagashimatravel/NagashimatravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nagashimatravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagashimatravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
